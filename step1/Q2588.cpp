19da14fc733fa20b1d8655baaff833cab9b1931c
xCrypt0r <fireintheholl@naver.com>
2020-08-13 23:27:24 +0900

    Add Q2588.cpp

---
+++ step1/Q2588.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << a * (b % 10) << endl;
+    cout << a * ((b % 100) / 10) << endl;
+    cout << a * (b / 100) << endl;
+    cout << a * b << endl;
+}
