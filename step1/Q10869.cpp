375a5e907eec5807e5e723931589ed7136da4c0c
xCrypt0r <fireintheholl@naver.com>
2020-08-13 23:20:36 +0900

    Add Q10869.cpp

---
+++ step1/Q10869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+}
