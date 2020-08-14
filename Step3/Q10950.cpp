739fce8ebd5321f5d537bbc37d81630f989b3e8b
SoBusted <fireintheholl@naver.com>
2020-08-14 12:29:42 +0000

    Add Q10950.cpp

---
+++ Step3/Q10950.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, a, b;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+		cin >> a >> b; 
+		cout << a + b << endl;
+    }
+}
