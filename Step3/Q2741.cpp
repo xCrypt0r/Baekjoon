21dafc46a27746193e52ca95c27f357faad60cf5
SoBusted <fireintheholl@naver.com>
2020-08-14 23:59:58 +0000

    Add Q2741.cpp

---
+++ Step3/Q2741.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cout << i << "\n";
+    }
+}
