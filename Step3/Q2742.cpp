34965d8929cc0c9e9d51acca6f713174e21e014c
SoBusted <fireintheholl@naver.com>
2020-08-15 00:07:54 +0000

    Add Q2742.cpp

---
+++ Step3/Q2742.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> N;
+
+    for (; N > 0; --N)
+    {
+        cout << N << "\n";
+    }
+}
