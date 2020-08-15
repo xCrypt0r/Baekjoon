1b28ee81ff4e84f4b85a9bae6668b4b8aab10376
SoBusted <fireintheholl@naver.com>
2020-08-15 06:47:24 +0000

    Q10951.cpp

---
+++ Step4/Q10951.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B;
+
+    while (cin >> A >> B)
+    {
+        cout << A + B << "\n";
+    }
+}
