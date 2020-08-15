7c3758e69add816322ed62a87fe0c7bf78bdc5e4
SoBusted <fireintheholl@naver.com>
2020-08-15 05:48:55 +0000

    Add Q11021.cpp

---
+++ Step3/Q11021.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T, A, B;
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin >> T;
+
+    for (int i = 1; i <= T; ++i)
+    {
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A + B << "\n";
+    }
+}
