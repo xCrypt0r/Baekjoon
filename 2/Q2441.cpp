b1c7c982072948fbb98913eaba836dc1cbec7641
SoBusted <fireintheholl@naver.com>
2020-08-17 06:04:07 +0000

    Add Q2441.cpp

---
+++ 2/Q2441.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            cout << ' ';
+        }
+
+        for (int j = 0; j < N - i; ++j)
+        {
+            cout << '*';
+        }
+
+        cout << '\n';
+    }
+}
