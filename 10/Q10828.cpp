62d131eab587bae720576e773da59afc955c88d1
SoBusted <fireintheholl@naver.com>
2020-08-19 13:48:49 +0000

    Add Q10828.cpp

---
+++ 10/Q10828.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    string cmd;
+    stack<int> s;
+
+    cin >> N;
+
+    for (; N > 0; --N)
+    {
+        cin >> cmd;
+
+        if (cmd == "push")
+        {
+            int num;
+
+            cin >> num;
+
+            s.push(num);
+        }
+        else if (cmd == "pop")
+        {
+            if (s.empty())
+            {
+                cout << -1 << "\n";
+            }
+            else
+            {
+                cout << s.top() << "\n";
+                s.pop();
+            }
+        }
+        else if (cmd == "size")
+        {
+            cout << s.size() << "\n";
+        }
+        else if (cmd == "empty")
+        {
+            cout << (s.empty() ? 1 : 0) << "\n";
+        }
+        else if (cmd == "top")
+        {
+            cout << (s.empty() ? -1 : s.top()) << "\n";
+        }
+    }
+}
