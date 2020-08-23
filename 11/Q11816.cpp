7e7b62a97084bdffa36ca22ed9879bb7d6349674
SoBusted <fireintheholl@naver.com>
2020-08-23 01:06:18 +0000

    Add Q11816.cpp

---
+++ 11/Q11816.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cin.unsetf(ios::dec);
+    cin.unsetf(ios::hex);
+    cin.unsetf(ios::oct);
+
+    int X;
+
+    cin >> X;
+    cout << X;
+}
