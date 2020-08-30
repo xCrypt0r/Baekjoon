ccb90d07ae6afe2fe8f5bfd074a297fd1dc6629f
SoBusted <fireintheholl@naver.com>
2020-08-30 13:53:09 +0000

    Add Q5339.cpp

---
+++ 5/Q5339.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "     /~\\" << "\n";
+    cout << "    ( oo|" << "\n";
+    cout << "    _\\=/_" << "\n";
+    cout << "   /  _  \\" << "\n";
+    cout << "  //|/.\\|\\\\ " << "\n";
+    cout << " ||  \\ /  ||" << "\n";
+    cout << "============" << "\n";
+    cout << "|          |" << "\n";
+    cout << "|          |" << "\n";
+    cout << "|          |" << "\n";
+}
