d046020a58f8ff72c4cd48c831ebeb3702f828db
SoBusted <fireintheholl@naver.com>
2020-08-30 03:52:06 +0000

    Add Q2445.cpp

---
+++ 2/Q2445.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cout << string(i, '*')
+             << string(N * 2 - i * 2, ' ')
+             << string(i, '*')
+             << '\n';
+    }
+
+    for (int i = N + 1; i <= N * 2 - 1; ++i)
+    {
+        cout << string(N * 2 - i, '*')
+             << string(i * 2 - N * 2, ' ')
+             << string(N * 2 - i, '*')
+             << '\n';
+    }
+}
