bdb987f81f918a6f21a33d609dd3772f7ae4f344
SoBusted <fireintheholl@naver.com>
2020-08-30 08:40:11 +0000

    Add Q10170.cpp

---
+++ 10/Q10170.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "NFC West       W   L  T" << '\n';
+    cout << "-----------------------" << '\n';
+    cout << "Seattle        13  3  0" << '\n';
+    cout << "San Francisco  12  4  0" << '\n';
+    cout << "Arizona        10  6  0" << '\n';
+    cout << "St. Louis      7   9  0" << '\n';
+    cout << '\n';
+    cout << "NFC North      W   L  T" << '\n';
+    cout << "-----------------------" << '\n';
+    cout << "Green Bay      8   7  1" << '\n';
+    cout << "Chicago        8   8  0" << '\n';
+    cout << "Detroit        7   9  0" << '\n';
+    cout << "Minnesota      5  10  1" << '\n';
+}
