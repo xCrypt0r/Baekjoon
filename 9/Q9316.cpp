68ce57c0ca7ec374fc4d88b0f748637b449c8917
SoBusted <fireintheholl@naver.com>
2020-09-05 03:47:39 +0000

    Add Q9316.cpp

---
+++ 9/Q9316.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cout << "Hello World, Judge " << i << "!\n";
+    }
+}
