45cd2e3a25145ab43467cba779821e9e7164bdc4
SoBusted <fireintheholl@naver.com>
2020-09-11 11:29:56 +0000

    Add Q2754.cpp

---
+++ 2/Q2754.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    map<string, double> grades = {
+        { "A+", 4.3 }, { "A0", 4.0 }, { "A-", 3.7 }, { "B+", 3.3 },
+        { "B0", 3.0 }, { "B-", 2.7 }, { "C+", 2.3 }, { "C0", 2.0 },
+        { "C-", 1.7 }, { "D+", 1.3 }, { "D0", 1.0 }, { "D-", 0.7 },
+        { "F", 0.0 }
+    };
+    string grade;
+
+    cin >> grade;
+    cout << fixed;
+    cout.precision(1);
+    cout << grades[grade];
+}
