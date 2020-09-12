899d0ea32c5d2289550d181f72416f5f5db0c087
SoBusted <fireintheholl@naver.com>
2020-09-12 10:32:28 +0000

    Add Q4402.cpp

---
+++ 4/Q4402.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+const int size = 6;
+const string soundexes [size] = { "BFPV", "CGJKQSXZ", "DT", "L", "MN", "R" };
+
+int get_soundex(char c);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string word;
+    
+    while (cin >> word)
+    {
+        int prev = 0, soundex = 0;
+
+        for (const auto& c : word)
+        {
+            soundex = get_soundex(c);
+
+            if (soundex == 0)
+            {
+                prev = 0;
+            }
+            else
+            {
+                if (soundex != prev)
+                {
+                    cout << soundex;
+
+                    prev = soundex;
+                }
+            }
+        }
+            
+        cout << "\n";
+    }
+}
+
+int get_soundex(char c)
+{
+    for (int i = 1; i <= size; ++i)
+    {
+        if (soundexes[i - 1].find(c) != -1)
+        {
+            return i;
+        }
+    }
+
+    return 0;
+}
