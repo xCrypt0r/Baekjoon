a6caf2779833a7e4d65f061eb083efdb2a825832
SoBusted <fireintheholl@naver.com>
2020-09-12 00:15:54 +0000

    Add Q9654.cpp

---
+++ 9/Q9654.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE" << "\n";
+	cout << "N2 Bomber      Heavy Fighter  Limited    21        " << "\n";	
+	cout << "J-Type 327     Light Combat   Unlimited  1         " << "\n";
+	cout << "NX Cruiser     Medium Fighter Limited    18        " << "\n";
+	cout << "N1 Starfighter Medium Fighter Unlimited  25        " << "\n";
+	cout << "Royal Cruiser  Light Combat   Limited    4        " << "\n";
+}
