3cf487f4913483ec2fed0a35c14fca5e65e77759
SoBusted <fireintheholl@naver.com>
2020-09-13 01:22:25 +0000

    Add Q1302.cpp

---
+++ 1/Q1302.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int N, max = 0;
+    map<string, int> books;
+    string title, bestseller;
+
+    cin >> N;
+
+    while (N--)
+    {
+        cin >> title;
+
+        books[title]++;
+    }
+
+    for (const auto& book : books)
+    {
+        if (book.second > max)
+        {
+		    max = book.second;;
+            bestseller = book.first;
+        }
+    }
+
+    cout << bestseller;
+}
