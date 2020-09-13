1703af0ff035ec7f63cbb4d067e2578062eaf75c
SoBusted <fireintheholl@naver.com>
2020-09-13 04:52:34 +0000

    Add Q1759.py

---
+++ 1/Q1759.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+L, C = map(int, input().split())
+used = sorted(list(map(str, input().split())))
+passwords = combinations(used, L)
+
+for password in passwords:
+    count = 0
+
+    for letter in password:
+        if letter in 'aeiou':
+            count += 1
+
+    if (count >= 1 and count <= L - 2):
+        print(''.join(password))
