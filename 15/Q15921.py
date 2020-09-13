90e0ccf120d6fc765e34056301f44d127d82e222
SoBusted <fireintheholl@naver.com>
2020-09-13 10:35:32 +0000

    Add Q15921.py

---
+++ 15/Q15921.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+if N == 0:
+    print('divide by zero')
+else:
+    time = list(map(int, input().split()))
+
+    if sum(time) == 0:
+        print('divide by zero')
+    else:
+        print('1.00')
