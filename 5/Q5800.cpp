4e0d2a6d392c0535502beba9f10484f6e857a28b
SoBusted <fireintheholl@naver.com>
2020-09-13 02:01:20 +0000

    Add Q5800.cpp

---
+++ 5/Q5800.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int K, N;
+
+    cin >> K;
+
+    for (int i = 1; i <= K; ++i)
+    {
+        cin >> N;
+
+        int score[N], max_gap = 0, gap;
+
+        for (int j = 0; j < N; ++j)
+        {
+            cin >> score[j];
+        }
+
+        sort(score, score + N, greater<int>());
+
+        for (int j = 0; j < N - 1; ++j)
+        {
+            if ((gap = score[j] - score[j + 1]) > max_gap)
+            {
+                max_gap = gap;
+            }
+        }
+
+        cout << "Class " << i << "\n";
+        cout << "Max " << score[0] << ", Min " << score[N - 1] << ", Largest gap " << max_gap << "\n";
+    }
+}
