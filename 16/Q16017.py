ea1a1aa641645d0dd864d317ac29e4b5242ac222
SoBusted <fireintheholl@naver.com>
2020-09-14 05:30:24 +0000

    Add Q16017.py

---
+++ 16/Q16017.py
@@ -0,0 +1,8 @@
+def main():
+    n = [int(input()) for _ in range(4)]
+
+    if n[0] > 7 < n[3] and n[1] == n[2]: print('ignore')
+    else: print('answer')
+
+if __name__ == '__main__':
+    main()
