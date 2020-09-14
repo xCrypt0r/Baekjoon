ce4d3e55003e8bef00874e479ad88958fda37bfc
SoBusted <fireintheholl@naver.com>
2020-09-14 05:18:22 +0000

    Add Q6778.py

---
+++ 6/Q6778.py
@@ -0,0 +1,10 @@
+def main():
+    a = int(input())
+    e = int(input())
+
+    if a >= 3 and e <= 4: print('TroyMartian')
+    if a <= 6 and e >= 2: print('VladSaturnian')
+    if a <= 2 and e <= 3: print('GraemeMercurian')
+
+if __name__ == '__main__':
+    main()
