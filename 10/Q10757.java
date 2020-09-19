ccce449e7365ec9280edab5e6e8560bc05397f7f
Archie <46510790+intelli-raccoon-dog@users.noreply.github.com>
2020-09-19 23:24:10 +0900

    Update Q10757.java

--- 10/Q10757.java
+++ 10/Q10757.java
@@ -11,7 +11,7 @@ public class Main {
         String small = new String();
         int[] result = new int[10001];
 
-        if(a[0].length() >= a[1].length()) {
+        if (a[0].length() >= a[1].length()) {
             a[0] = "0" + a[0];
             big = a[0];
             small = a[1];
@@ -19,16 +19,16 @@ public class Main {
             a[1] = "0" + a[1];
             big = a[1];
             small = a[0];
-        } //Comparison
+        }
 
-        int i = big.length() -1, j = small.length() -1;
+        int i = big.length() - 1, j = small.length() - 1;
 
-        while(i != -1) {
+        while (i != -1) {
 
-            int n_b = Integer.parseInt(String.valueOf(big.charAt(i)), 10); //bigint parse
+            int n_b = Integer.parseInt(String.valueOf(big.charAt(i)), 10);
 
-            if(j >= 0) {
-                int n_s = Integer.parseInt(String.valueOf(small.charAt(j)), 10); //smallint parse
+            if (j >= 0) {
+                int n_s = Integer.parseInt(String.valueOf(small.charAt(j)), 10);
                 result[i] = result[i] + n_b + n_s;
             } else {
                 result[i] = result[i] + n_b;
@@ -36,21 +36,22 @@ public class Main {
 
             if (result[i] >= 10) {
                 result[i] = result[i] - 10;
-                result[i-1] = 1;
-            } // >= 10
+                result[i - 1] = 1;
+            }
 
-            i--; j--;
+            i--;
+            j--;
         }
 
-        for(int k = 0; k < big.length(); k++) {
-            if(k == 0){
-                if(result[0] == 0){
-                    //When first number is 0
+        for (int k = 0; k < big.length(); k++) {
+            if (k == 0) {
+                if (result[0] == 0) {
+                    continue;
                 } else {
                     System.out.print(result[k]);
                 }
-            } else {	
-                System.out.print(result[k]);	
+            } else {
+                System.out.print(result[k]);
             }
         }
 
