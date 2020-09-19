ad7138c61a90394039b8750e5ea8ef1f55aee5d1
SoBusted <fireintheholl@naver.com>
2020-09-19 08:26:21 +0000

    Add Q16175.py

---
+++ 16/Q16175.py
@@ -0,0 +1,13 @@
+def main():
+    for _ in range(int(input())):
+        N, M = map(int, input().split())
+        votes = [0] * N
+
+        for _ in range(M):
+            for i, v in enumerate(map(int, input().split())):
+                votes[i] += v
+
+        print(votes.index(max(votes)) + 1)
+
+if __name__ == '__main__':
+    main()
