56540ceba14d0e15363d4cbae0292e9e37799ef4
SoBusted <fireintheholl@naver.com>
2020-09-19 14:30:56 +0000

    Add Q15025.py

---
+++ 15/Q15025.py
@@ -0,0 +1,12 @@
+def main():
+    l, r = map(int, input().split())
+
+    if l == r == 0:
+        print('Not a moose')
+    elif l == r:
+        print(f'Even {l * 2}')
+    else:
+        print(f'Odd {max(l, r) * 2}')
+
+if __name__ == '__main__':
+    main()
