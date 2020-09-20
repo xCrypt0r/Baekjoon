269744cc56d1ef0677e6e0e6cbb2c44bed00c35c
SoBusted <fireintheholl@naver.com>
2020-09-20 07:20:31 +0000

    Add Q8716.py

---
+++ 8/Q8716.py
@@ -0,0 +1,11 @@
+def main():
+    x1, y1, x2, y2 = map(int, input().split())
+    x3, y3, x4, y4 = map(int, input().split())
+
+    if x2 < x3 or x1 > x4 or y2 > y3 or y1 < y4:
+        print(0)
+    else:
+        print((min(x2, x4) - max(x1, x3)) * (min(y1, y3) - max(y2, y4)))
+
+if __name__ == '__main__':
+    main()
