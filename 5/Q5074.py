8ae14565482aff2352202b2f22f51d4cf1564595
SoBusted <fireintheholl@naver.com>
2020-09-24 08:47:27 +0000

    Add Q5074.py

---
+++ 5/Q5074.py
@@ -0,0 +1,30 @@
+import sys
+
+def main():
+    while True:
+        start, duration = input().split()
+
+        if start == duration == '00:00':
+            break
+
+        h1, m1 = map(int, start.split(':'))
+        h2, m2 = map(int, duration.split(':'))
+        m = m1 + m2
+        h = h1 + h2
+        n = 0
+
+        if m >= 60:
+            m -= 60
+            h += 1
+
+        if h >= 24:
+            n += h // 24
+            h %= 24
+
+        if n > 0:
+            print(f'{h:02}:{m:02} +{n}')
+        else:
+            print(f'{h:02}:{m:02}')
+
+if __name__ == '__main__':
+    main()
