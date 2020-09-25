7eecc6d338a623529785f787d30cda9c54da7b58
SoBusted <fireintheholl@naver.com>
2020-09-25 08:12:02 +0000

    Add Q15624.py

---
+++ 15/Q15624.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+    fibonacci = [0, 1]
+
+    for i in range(2, n + 1):
+        fibonacci.append((fibonacci[i - 1] + fibonacci[i - 2]) % 1000000007)
+
+    print(fibonacci[n])
+
+if __name__ == '__main__':
+    main()
