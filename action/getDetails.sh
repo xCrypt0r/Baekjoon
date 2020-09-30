ef70c8c8b7efe9b362153382125b7f5feb50fb9b
SoBusted <fireintheholl@naver.com>
2020-09-30 23:57:40 +0000

    Update getDetails.sh

--- action/getDetails.sh
+++ action/getDetails.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+src="../src"
+ext="$1"
 lines=$(
-    git ls-files ../src \
-    | grep ".*\.$1" \
+    git ls-files $src \
+    | grep ".*\.$ext" \
     | xargs cat \
     | wc -l \
 )
 size=$( \
-    git ls-files ../src \
-    | grep ".*\.$1" \
+    git ls-files $src \
+    | grep ".*\.$ext" \
     | xargs du -bc \
     | tail -1 \
     | cut -f1 \
 )
 
-echo "$lines $size"
+echo $lines $size
