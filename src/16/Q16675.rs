726a57185c956c0605d17314d6fcf0a17fcaa5b3
SoBusted <fireintheholl@naver.com>
2020-10-01 05:37:53 +0000

    Add Q16675.rs

---
+++ src/16/Q16675.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+
+use std::io;
+use std::collections::HashMap;
+
+fn main() {
+    let mut s = String::new();
+    
+    io::stdin().read_line(&mut s).unwrap();
+
+    let v: Vec<&str> = s.split_whitespace().collect();
+    let (ML, MR, TL, TR) = (v[0], v[1], v[2], v[3]);
+    let mut map = HashMap::new();
+    
+    map.insert("R", "P");
+    map.insert("P", "S");
+    map.insert("S", "R");
+
+    if ML == MR && [TL, TR].contains(&map[ML]) {
+        println!("TK");
+    }
+    else if TL == TR && [ML, MR].contains(&map[TL]) {
+        println!("MS");
+    }
+    else {
+        println!("?");
+    }
+}
