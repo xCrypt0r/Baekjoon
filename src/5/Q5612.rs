551808987a0ded56fecf3089a030f1c18748fbea
SoBusted <fireintheholl@naver.com>
2020-10-02 06:56:47 +0000

    Update Q5612.rs

--- src/5/Q5612.rs
+++ src/5/Q5612.rs
@@ -1,4 +1,4 @@
-#![allow(non_snake_case)]
+use std::cmp;
 
 macro_rules! get_line {
     ( $( $t: ty ),+ ) => {
@@ -13,8 +13,6 @@ macro_rules! get_line {
         }
     }
 }
-
-use std::cmp;
 
 fn main() {
     let n = get_line!(i32);
