60aef2ec47b142b10a56194dc336fe93faa84648
SoBusted <fireintheholl@naver.com>
2020-10-02 12:18:59 +0000

    Add Q13363.rs

---
+++ src/13/Q13363.rs
@@ -0,0 +1,27 @@
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+  
+            std::io::stdin().read_line(&mut line).unwrap();
+  
+                let mut iter = line.split_whitespace();
+  
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let n1 = get_line!(i32);
+    let n2 = get_line!(i32);
+    let mut res = n2 - n1;
+
+    if res > 180 {
+        res -= 360;
+    } else if res <= -180 {
+        res += 360;
+    }
+
+    println!("{}", res);
+}
