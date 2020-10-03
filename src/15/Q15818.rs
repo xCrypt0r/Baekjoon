e9f3292e982ee786136e0497b6738aae5babd263
SoBusted <fireintheholl@naver.com>
2020-10-03 05:00:03 +0000

    Add Q15818.rs

---
+++ src/15/Q15818.rs
@@ -0,0 +1,44 @@
+#![allow(non_snake_case)]
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+  
+            std::io::stdin().read_line(&mut line).unwrap();
+  
+            let mut iter = line.split_whitespace();
+  
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+macro_rules! get_list {
+    ( $t: ty ) => {
+        {
+            let mut line = String::new();
+
+            std::io::stdin().read_line(&mut line).unwrap();
+
+            let list: Vec<$t> = line
+                .split_whitespace()
+                .map(|s| s.parse::<$t>().unwrap())
+                .collect();
+
+            list
+        }
+    }
+}
+
+fn main() {
+    let (_, M) = get_line!(i64, i64);
+    let mut res = 1;
+    let a = get_list!(i64);
+
+    for i in 0..a.len() {
+        res = (res * (a[i] % M)) % M;
+    }
+
+    println!("{}", res);
+}
