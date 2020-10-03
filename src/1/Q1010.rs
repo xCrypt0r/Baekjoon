003fd5efe1c99bcb8aa45fb59ba79804d33ff1a3
SoBusted <fireintheholl@naver.com>
2020-10-03 08:08:39 +0000

    Add Q1010.rs

---
+++ src/1/Q1010.rs
@@ -0,0 +1,37 @@
+#![allow(non_snake_case)]
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+  
+            std::io::stdin().read_line(&mut line).unwrap();
+  
+            let mut iter = line.split_whitespace();
+  
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let T = get_line!(i32);
+    let mut memo = vec![vec![0; 31]; 31];
+
+    for i in 0..31 {
+        memo[i][i] = 1;
+        memo[i][0] = 1;
+    }
+
+    for i in 1..31 {
+        for j in 1..31 {
+            memo[i][j] = memo[i - 1][j] + memo[i - 1][j - 1];
+        }
+    }
+
+    for _ in 0..T {
+        let (N, M) = get_line!(usize, usize);
+
+        println!("{}", memo[M][N]);
+    }
+}
