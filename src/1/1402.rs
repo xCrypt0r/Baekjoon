cbb4f8bac2dedbc8973d3859469367642899db22
SoBusted <fireintheholl@naver.com>
2020-10-08 04:38:21 +0000

    Add 1402.rs

---
+++ src/1/1402.rs
@@ -0,0 +1,29 @@
+#![allow(non_snake_case)]
+
+use std::io::{self, Write};
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let stdout = io::stdout();
+    let mut out = io::BufWriter::new(stdout.lock());
+    let T = get_line!(i32);
+
+    for _ in 0..T {
+        let (_, _) = get_line!(i32, i32);
+
+        writeln!(out, "yes").unwrap();
+    }
+}
