0f2a06abcc1bc220de90272674a84fc9cdfbfa6c
SoBusted <fireintheholl@naver.com>
2020-10-08 05:04:15 +0000

    Add 15547.rs

---
+++ src/15/15547.rs
@@ -0,0 +1,21 @@
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            std::io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let (a, b) = get_line!(usize, usize);
+    let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis lectus sit amet ante suscipit tempor. Aenean dapibus finibus tortor vitae facilisis. Nulla facilisi. Donec suscipit ante a sapien gravida eleifend. Curabitur in lectus ut enim semper venenatis. Aenean elit nibh, hendrerit nec nunc in, dictum cursus dolor. Aenean eget lectus risus. Cras sollicitudin quam vitae augue mollis bibendum. Aliquam euismod vulputate dolor at tristique. Praesent elementum pretium velit non interdum. Nunc posuere faucibus magna id pretium. Maecenas congue, velit eget semper porttitor, tellus tortor porttitor dui, tristique lobortis quam sem ac odio.";
+    let v: Vec<&str> = data.split(" ").collect();
+
+    println!("{}", &v[a - 1..b].join(" "));
+}
