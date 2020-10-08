57816d28a2360a5b62fa0415003c6dcf56c26c34
SoBusted <fireintheholl@naver.com>
2020-10-08 06:33:56 +0000

    Add 14563.rs

---
+++ src/14/14563.rs
@@ -0,0 +1,51 @@
+use std::io::{self, Write};
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+macro_rules! get_list {
+    ( $t: ty ) => {
+        {
+            let mut line = String::new();
+
+            io::stdin().read_line(&mut line).unwrap();
+
+            let list: Vec<$t> = line
+                .split_whitespace()
+                .map(|s| s.parse::<$t>().unwrap())
+                .collect();
+
+            list
+        }
+    }
+}
+
+fn main() {
+    let stdout = io::stdout();
+    let mut out = io::BufWriter::new(stdout.lock());
+    let _ = get_line!(i32);
+    let v = get_list!(i64);
+
+    for n in v {
+        let mut sum = 0;
+
+        for i in 1..n {
+            if n % i == 0 {
+                sum += i;
+            }
+        }
+
+        writeln!(out, "{}", if sum == n { "Perfect" } else if sum > n { "Abundant" } else { "Deficient" }).unwrap();
+    }
+}
