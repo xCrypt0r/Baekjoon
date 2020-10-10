a51179d71a93cb4e43fc05832ca3ac8faa39b2d9
SoBusted <fireintheholl@naver.com>
2020-10-10 02:27:25 +0000

    Add 20004.rs

---
+++ src/20/20004.rs
@@ -0,0 +1,29 @@
+#![allow(non_snake_case)]
+
+use std::io::{self, Write};
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let stdout = io::stdout();
+    let mut out = io::BufWriter::new(stdout.lock());
+    let A = get_line!(usize);
+
+    for i in 1..=A {
+        if 30 % (i + 1) == 0 {
+            writeln!(out, "{}", i).unwrap();
+        }
+    }
+}
