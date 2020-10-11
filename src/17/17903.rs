4c87723b08d0507cf2b1cf4a8e5e705a1ef84b6c
SoBusted <fireintheholl@naver.com>
2020-10-11 11:18:02 +0000

    Add 17903.rs

---
+++ src/17/17903.rs
@@ -0,0 +1,19 @@
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            std::io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let (m, _) = get_line!(i32, i32);
+
+    println!("{}", if m >= 8 { "satisfactory" } else { "unsatisfactory" });
+}
