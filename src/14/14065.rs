c27d06fb41e68fd92c2285a3d67a049f5469dcce
SoBusted <fireintheholl@naver.com>
2020-10-11 13:39:25 +0000

    Add 14065.rs

---
+++ src/14/14065.rs
@@ -0,0 +1,19 @@
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            std::io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let x = get_line!(f64);
+
+    println!("{:.6}", 100.0 / ((1.609344 / 3.785411784) * x));
+}
