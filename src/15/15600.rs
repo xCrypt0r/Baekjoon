9242f086436961bfb58a838d11f794fea3dad7c3
SoBusted <fireintheholl@naver.com>
2020-10-12 14:05:02 +0000

    Add 15600.rs

---
+++ src/15/15600.rs
@@ -0,0 +1,21 @@
+use std::cmp;
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            std::io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let n = get_line!(i32);
+
+    println!("{}", cmp::max(n - 2, 1));
+}
