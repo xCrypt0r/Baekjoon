ad922f8b67638ad02ce646d32d00c53954cacb61
SoBusted <fireintheholl@naver.com>
2020-10-13 13:43:23 +0000

    Add 3053.rs

---
+++ src/3/3053.rs
@@ -0,0 +1,24 @@
+#![allow(non_snake_case)]
+
+use std::{io, f64};
+
+macro_rules! get_line {
+    ( $( $t: ty ),+ ) => {
+        {
+            let mut line = String::new();
+
+            io::stdin().read_line(&mut line).unwrap();
+
+            let mut iter = line.split_whitespace();
+
+            ( $( iter.next().unwrap().parse::<$t>().unwrap() ),+ )
+        }
+    }
+}
+
+fn main() {
+    let R = get_line!(i32);
+
+    println!("{:.6}", R.pow(2) as f64 * f64::consts::PI);
+    println!("{:.6}", R.pow(2) as f64 * 2.0);
+}
