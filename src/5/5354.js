1503d2b26f817a69fd1e7cb427b241542740ba1a
SoBusted <fireintheholl@naver.com>
2020-10-14 05:48:29 +0000

    Add 5354.js

---
+++ src/5/5354.js
@@ -0,0 +1,25 @@
+const  fs = require('fs');
+
+function main() {
+    let [T, ...sizes] = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+    for (let size of sizes) {
+        if (size < 3) {
+            for (let i = 0; i < size; i++) {
+                console.log('#'.repeat(size));
+            }
+
+            console.log();
+        } else {
+            console.log('#'.repeat(size));
+
+            for (let i = 0; i < size - 2; i++) {
+                console.log(`#${'J'.repeat(size - 2)}#`);
+            }
+
+            console.log('#'.repeat(size) + '\n');
+        }
+    }
+}
+
+main();
