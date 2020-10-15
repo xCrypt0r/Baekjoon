3b045413eeffa0846cf4c2249eaa8ffb89f8a6a3
SoBusted <fireintheholl@naver.com>
2020-10-15 07:22:53 +0000

    Add 13777.js

---
+++ src/13/13777.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function main() {
+    let numbers = fs.readFileSync('../stdin').toString().split('\n').map(x => +x);
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] == 0) {
+            break;
+        }
+
+        let [start, end] = [1, 50];
+        let output = [];
+
+        while (start <= end) {
+            let middle = Math.floor((start + end) / 2);
+
+            output.push(middle);
+
+            if (middle > numbers[i]) {
+                end = middle - 1;
+            } else if (middle < numbers[i]) {
+                start = middle + 1;
+            } else {
+                break;
+            }
+        }
+
+        console.log(output.join(' '));
+    }
+}
+
+main();
