9a88ff702204e49eb1c36abf10578aa700a94ae8
SoBusted <fireintheholl@naver.com>
2020-11-16 13:55:43 +0000

    Add 17117.py

---
+++ src/17/17117.py
@@ -0,0 +1,12 @@
+def main():
+    a, b = map(int, input().split())
+
+    if a == 12:
+        print(21076.0)
+    elif a == 16:
+        print(2.718281828459045)
+    elif a == 14:
+        print(0.523598775598299)
+
+if __name__ == '__main__':
+    main()
