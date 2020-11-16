9cd3b075da18ee9d4ca7b93152e68934c68c67e5
SoBusted <fireintheholl@naver.com>
2020-11-16 15:05:27 +0000

    Add 20053.js

---
+++ src/20/20053.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function main() {
+    let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+    let T = +input.shift(),
+        arr = [];
+
+    for (let i = 0; i < T; i++) {
+        let max = -1000000,
+            min = 1000000;
+
+        arr = input[(i << 1) + 1].split(' ').map(x => parseInt(x));
+
+        for (let j = 0, len = arr.length; j < len; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+            }
+
+            if (arr[j] > max) {
+                max = arr[j];
+            }
+        }
+
+        console.log(min, max);
+    }
+}
+
+main();
