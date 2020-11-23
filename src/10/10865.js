cb34a410133a44b44aeb0653d7c7f8f4d42d00cf
SoBusted <fireintheholl@naver.com>
2020-11-23 10:15:45 +0000

    Add 10865.js

---
+++ src/10/10865.js
@@ -0,0 +1,30 @@
+/**
+ * 10865. 친구 친구
+ * 
+ * 작성자: xCrypt0r
+ * 언어: node.js
+ * 사용 메모리: 180,136 KB
+ * 소요 시간: 1,308 ms
+ * 해결 날짜: 2020년 11월 23일
+ */
+
+const fs = require('fs');
+
+function main() {
+    let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+    let [N, M] = input.shift().split(' ').map(x => +x),
+        count = new Array(N + 1).fill(0);
+
+    input = input.map(x => x.split(' ').map(y => +y));
+
+    for (let i = 0; i < M; i++) {
+        let [a, b] = input[i];
+
+        count[a]++;
+        count[b]++;
+    }
+
+    console.log(count.slice(1).join('\n'));
+}
+
+main();
