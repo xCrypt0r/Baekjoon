8cf445b6296d00cdd55bf8f3a1f6ae8a523e538f
SoBusted <fireintheholl@naver.com>
2020-11-25 01:04:10 +0000

    Add 14723.js

---
+++ src/14/14723.js
@@ -0,0 +1,21 @@
+/**
+ * 14723. 이산수학 과제
+ * 
+ * 작성자: xCrypt0r
+ * 언어: node.js
+ * 사용 메모리: 8,312 KB
+ * 소요 시간: 140 ms
+ * 해결 날짜: 2020년 11월 25일
+ */
+
+const fs = require('fs');
+
+function main() {
+    let N = +fs.readFileSync('/dev/stdin').toString();
+    let x = ~~(((Math.sqrt((8 * N) - 7) - 1) / 2) + 1),
+        y = ~~(x * (x + 1)) / 2;
+
+    console.log(`${y - N + 1} ${N - y + x}`);
+}
+
+main();
