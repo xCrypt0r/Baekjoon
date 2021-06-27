4db3ef7539c34cb6732fd8749b09127f66550cfd
xCrypt0r <legacy5877@gmail.com>
2021-06-27 22:26:36 +0000

    Add 21300.cpp

---
+++ src/21/21300.cpp
@@ -0,0 +1,21 @@
+/**
+ * 21300. Bottle Return
+ * 
+ * 작성자: xCrypt0r
+ * 언어: C++14
+ * 사용 메모리: 2,020 KB
+ * 소요 시간: 0 ms
+ * 해결 날짜: 2021년 6월 28일
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f;
+
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << (a + b + c + d + e + f) * 5 << endl;
+}
