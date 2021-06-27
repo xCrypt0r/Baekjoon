fc9bbb72201a4c74b9c8edfa766c56e0839098c6
xCrypt0r <legacy5877@gmail.com>
2021-06-27 23:29:42 +0000

    Add 21598.cpp

---
+++ src/21/21598.cpp
@@ -0,0 +1,25 @@
+/**
+ * 21598. SciComLove
+ * 
+ * 작성자: xCrypt0r
+ * 언어: C++14
+ * 사용 메모리: 2,020 KB
+ * 소요 시간: 0 ms
+ * 해결 날짜: 2021년 6월 28일
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        cout << "SciComLove" << endl;
+    }
+}
