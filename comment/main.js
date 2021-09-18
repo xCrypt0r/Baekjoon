3ba138e87a4f9462e12a98c0a350a990c8f5dc7e
xCrypt0r <legacy5877@gmail.com>
2021-09-19 07:56:32 +0900

    Add support for Assembly

--- comment/main.js
+++ comment/main.js
@@ -56,6 +56,11 @@ function setComment(target, { id, title, memory, time, language, date }) {
 
             break;
 
+        case '.asm':
+            template = fs.readFileSync('templates/comment_asm.txt', 'utf8');
+
+            break;
+
         case '.lua':
             template = fs.readFileSync('templates/comment_lua.txt', 'utf8');
 
