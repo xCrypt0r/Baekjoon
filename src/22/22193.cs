1e9ebf16a29507e3baa94845186c59b5e1332170
xCrypt0r <legacy5877@gmail.com>
2021-09-19 23:34:23 +0900

    Add 22193.cs

---
+++ src/22/22193.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * 22193. Multiply
+ *
+ * 작성자: xCrypt0r
+ * 언어: C# 9.0 (.NET)
+ * 사용 메모리: 21,400 KB
+ * 소요 시간: 880 ms
+ * 해결 날짜: 2021년 9월 19일
+ */
+
+using System;
+using System.Numerics;
+
+class Program {
+    public static void Main() {
+        Console.ReadLine();
+
+        BigInteger A = BigInteger.Parse(Console.ReadLine());
+        BigInteger B = BigInteger.Parse(Console.ReadLine());
+
+        Console.WriteLine(A * B);
+    }
+}
