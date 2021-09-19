b89c077c39c1105638521097524164c82f216db0
xCrypt0r <legacy5877@gmail.com>
2021-09-19 18:42:53 +0900

    Add 1001.pas

---
+++ src/1/1001.pas
@@ -0,0 +1,16 @@
+{**
+ * 1001. A-B
+ *
+ * 작성자: xCrypt0r
+ * 언어: Pascal
+ * 사용 메모리: 336 KB
+ * 소요 시간: 0 ms
+ * 해결 날짜: 2021년 9월 19일
+ *}
+
+var
+  a, b : Integer;
+begin
+  Readln(a, b);
+  Writeln(a - b);
+end.
