8b625320b1f28cd9817be77f4ecec7e6efdd51f9
xCrypt0r <legacy5877@gmail.com>
2021-09-22 13:05:01 +0900

    Add 20944.cs

---
+++ src/20/20944.cs
@@ -0,0 +1,19 @@
+/**
+ * 20944. 팰린드롬 척화비
+ *
+ * 작성자: xCrypt0r
+ * 언어: C# 9.0 (.NET)
+ * 사용 메모리: 8,376 KB
+ * 소요 시간: 292 ms
+ * 해결 날짜: 2021년 9월 22일
+ */
+
+using System;
+
+class Program {
+    public static void Main() {
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(new String('x', N));
+    }
+}
