99323080ff23591232863c51e278aba8ddb7542d
xCrypt0r <legacy5877@gmail.com>
2021-09-24 15:54:32 +0900

    Add 13419.cs

---
+++ src/13/13419.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        var T = int.Parse(Console.ReadLine());
+        var res = new String[T];
+
+        for (var i = 0; i < T; i++)
+        {
+            var input = Console.ReadLine();
+            var res1 = "";
+            var res2 = "";
+
+            if (input.Length % 2 == 1)
+            {
+                input += input;
+            }
+
+            for (var j = 0; j < input.Length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    res1 += input[j];
+                }
+                else
+                {
+                    res2 += input[j];
+                }
+            }
+
+            res[i] = res1 + "\n" + res2;
+        }
+
+        Console.WriteLine(new StringBuilder(string.Join("\n", res)));
+    }
+}
