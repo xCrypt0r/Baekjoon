0d8f83acf786601f919282ced94da36e00ef6895
xCrypt0r <legacy5877@gmail.com>
2021-09-28 21:02:26 +0900

    Add 20833.sh

---
+++ src/20/20833.sh
@@ -0,0 +1,20 @@
+: '
+20833. Kuber
+
+작성자: xCrypt0r
+언어: Bash
+사용 메모리: 18,252 KB
+소요 시간: 8 ms
+해결 날짜: 2021년 9월 28일
+'
+
+read N
+
+res=0
+
+for ((i=1; i<=N; i++))
+do
+    res=$((res + i * i * i))
+done
+
+echo $res
