833c33d5a81a70924b8da93a4a44b910e12910ae
xCrypt0r <legacy5877@gmail.com>
2021-09-29 00:08:24 +0900

    Add 2752.sh

---
+++ src/2/2752.sh
@@ -0,0 +1,34 @@
+: '
+2752. 세수정렬
+
+작성자: xCrypt0r
+언어: Bash
+사용 메모리: 18,280 KB
+소요 시간: 8 ms
+해결 날짜: 2021년 9월 28일
+'
+
+qsort() {
+    arr=("$@")
+
+    local i j k
+
+    for ((i = 1; i < ${#arr[@]}; i++))
+    do
+        k=${arr[i]}
+        j=$((i - 1))
+
+        while [[ $j -ge 0 && ${arr[j]} -gt $k ]]
+        do
+            arr[$((j + 1))]=${arr[j]}
+            ((j--))
+        done
+
+        arr[$((j + 1))]=$k
+    done
+
+    echo ${arr[@]}
+}
+
+read x y z
+qsort $x $y $z
