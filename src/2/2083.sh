c157c941667544f1b29539e089c373978b7be437
xCrypt0r <legacy5877@gmail.com>
2021-09-29 00:46:32 +0900

    Add 2083.sh

---
+++ src/2/2083.sh
@@ -0,0 +1,30 @@
+: '
+2083. 럭비 클럽
+
+작성자: xCrypt0r
+언어: Bash
+사용 메모리: 18,288 KB
+소요 시간: 8 ms
+해결 날짜: 2021년 9월 29일
+'
+
+main() {
+    while true
+    do
+        read name age weight
+
+        if [ "$name" = "#" ] && [ $age -eq 0 ] && [ $weight -eq 0 ]
+        then
+            break
+        fi
+
+        if [ $age -gt 17 ] || [ $weight -ge 80 ]
+        then
+            echo "$name Senior"
+        else
+            echo "$name Junior"
+        fi
+    done
+}
+
+main
