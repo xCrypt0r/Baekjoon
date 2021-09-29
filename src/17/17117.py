5dbd10f372abea83cac63c8ebbc2e3687f174a29
xCrypt0r <legacy5877@gmail.com>
2021-09-30 04:08:44 +0900

    Update 17117.py

--- src/17/17117.py
+++ src/17/17117.py
@@ -3,20 +3,13 @@
 
 작성자: xCrypt0r
 언어: Python 3
-사용 메모리: 29,088 KB
+사용 메모리: 29,200 KB
 소요 시간: 68 ms
-해결 날짜: 2020년 11월 16일
+해결 날짜: 2021년 9월 30일
 """
 
 def main():
-    a, b = map(int, input().split())
-
-    if a == 12:
-        print(21076.0)
-    elif a == 16:
-        print(2.718281828459045)
-    elif a == 14:
-        print(0.523598775598299)
+    print(0)
 
 if __name__ == '__main__':
     main()
