0a6e02552c46de72f02bb0c67759f23f1279bd5a
xCrypt0r <legacy5877@gmail.com>
2021-10-09 15:28:31 +0900

    Fix issue that extension doesn't match exactly

--- action/getDetails.sh
+++ action/getDetails.sh
@@ -4,13 +4,13 @@ src="../src"
 ext="$1"
 lines=$(
     git ls-files $src \
-    | grep ".*\.$ext" \
+    | grep ".*\.$ext$" \
     | xargs cat \
     | wc -l \
 )
 size=$( \
     git ls-files $src \
-    | grep ".*\.$ext" \
+    | grep ".*\.$ext$" \
     | xargs du -bc \
     | tail -1 \
     | cut -f1 \
