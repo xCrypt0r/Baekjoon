2be4625f6b5f530fa5bafef362206104f4c992f2
xCrypt0r <legacy5877@gmail.com>
2021-10-11 15:13:32 +0900

    Add 10718.rb

---
+++ src/10/10718.rb
@@ -0,0 +1,17 @@
+=begin
+10718. We love kriii
+
+작성자: xCrypt0r
+언어: Ruby
+사용 메모리: 97,812 KB
+소요 시간: 148 ms
+해결 날짜: 2021년 10월 11일
+=end
+
+def main
+    2.times {
+        puts "강한친구 대한육군"
+    }
+end
+
+main
