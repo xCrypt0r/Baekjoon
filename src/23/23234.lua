07c3d1d2e94330099136b3ce33d54e65b86d1c48
xCrypt0r <legacy5877@gmail.com>
2021-10-11 15:33:37 +0900

    Add 23234.lua

---
+++ src/23/23234.lua
@@ -0,0 +1,15 @@
+--[[
+23234. The World Responds
+
+작성자: xCrypt0r
+언어: Lua
+사용 메모리: 9,764 KB
+소요 시간: 0 ms
+해결 날짜: 2021년 10월 11일
+--]]
+
+function main()
+    print("The world says hello!")
+end
+
+main()
