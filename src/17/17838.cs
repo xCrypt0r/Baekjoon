4413e6555d325eb2c7fd84ae4124ab0830062eb6
xCrypt0r <legacy5877@gmail.com>
2021-10-16 23:25:15 +0900

    Update 17838.cs

--- src/17/17838.cs
+++ src/17/17838.cs
@@ -3,8 +3,8 @@
  *
  * 작성자: xCrypt0r
  * 언어: C# 9.0 (.NET)
- * 사용 메모리: 7,076 KB
- * 소요 시간: 128 ms
+ * 사용 메모리: 7,092 KB
+ * 소요 시간: 108 ms
  * 해결 날짜: 2021년 10월 16일
  */
 
@@ -16,13 +16,11 @@ class Program
     public static void Main()
     {
         var T = int.Parse(Console.ReadLine());
-        var rgx = new Regex(@"^(.)\1(.)\2\1\2\2$");
+        var rgx = new Regex(@"^(.)\1((?!\1).)\2\1\2\2$");
 
         while (T-- > 0)
         {
-            var S = Console.ReadLine();
-
-            Console.WriteLine(S.Length == 7 && S[0] != S[2] && rgx.IsMatch(S) ? 1 : 0);
+            Console.WriteLine(rgx.IsMatch(Console.ReadLine()) ? 1 : 0);
         }
     }
 }
