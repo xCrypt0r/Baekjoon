45019b17a4d9a352646449dc0438e4d28fe4d4cb
xCrypt0r <legacy5877@gmail.com>
2021-10-22 04:10:21 +0900

    Add 14906.pl

---
+++ src/14/14906.pl
@@ -0,0 +1,29 @@
+=pod
+14906. 스러피
+
+작성자: xCrypt0r
+언어: Perl
+사용 메모리: 17,376 KB
+소요 시간: 8 ms
+해결 날짜: 2021년 10월 22일
+=cut
+
+sub main {
+    my $re_slump = qr/^([D|E]F+)+G$/;
+    my $re_slimp = qr/AH|A([D|E]F+)+GC|AB(?0)C/;
+
+    <STDIN>;
+    print "SLURPYS OUTPUT\n";
+
+    while (my $s = <STDIN>) {
+        if ($s =~ $re_slimp && index($s, $&) == 0 && substr($s, length($&)) =~ $re_slump) {
+            print "YES\n";
+        } else {
+            print "NO\n";
+        }
+    }
+
+    print "END OF OUTPUT\n";
+}
+
+main
