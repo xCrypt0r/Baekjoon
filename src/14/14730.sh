516aafb29466ae442690e4f3d56829a11b1d9245
xCrypt0r <legacy5877@gmail.com>
2021-11-03 08:02:59 +0900

    Add 14730.sh

---
+++ src/14/14730.sh
@@ -0,0 +1,25 @@
+: '
+14730. 謎紛芥索紀 (Small)
+
+작성자: xCrypt0r
+언어: Bash
+사용 메모리: 18,276 KB
+소요 시간: 24 ms
+해결 날짜: 2021년 11월 3일
+'
+
+main() {
+    local N C K res=0
+
+    read N
+
+    for (( i = 0; i < N; i++ )); do
+        read C K
+
+        (( S += C * K ))
+    done
+
+    echo $S
+}
+
+main
